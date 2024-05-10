496b06515a6ff55c001cdf40a8ab408cf71143d3
praveen22s <masspraveen2k4@gmail.com>
2024-05-10 20:42:56 +0530

    last

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Task_report_upd from "./Compnents/Task_report_upd.jsx";
+function App() {
+  return (
+    <div className="App">
+      <Task_report_upd/>
+    </div>
+  );
+}
+
+export default App;
